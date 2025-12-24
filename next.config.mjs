833b18cf72c6d70a9b64f313e867e1039b524c51
carlos montoya bacigalupo <cacomontoya@gmail.com>
2025-12-24 04:19:09 -0300

    Add ESLint configuration to ignore during builds

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   
   // Headers de seguridad
   async headers() {
